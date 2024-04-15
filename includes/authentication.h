0b18fefef3699c59f56b2f20c09623f69bda7e03
Ningchen123 <159767215+Ningchen123@users.noreply.github.com>
2024-04-15 11:31:57 -0700

    Initial commit

---
+++ includes/authentication.h
@@ -0,0 +1,12 @@
+// authentication.h
+#ifndef AUTHENTICATION_H
+#define AUTHENTICATION_H
+
+#include <stdio.h>
+
+// 函数原型
+void login(char* username, char* password);
+void registerUser(char* username, char* password);
+
+#endif
+
---
+++ src/main.c
@@ -0,0 +1,10 @@
+// main.c
+#include <stdio.h>
+#include "authentication.h"  // 假设你将创建这个头文件来处理认证
+
+int main() {
+    printf("Welcome to the authentication system\\n");
+    // 这里添加更多的逻辑
+    return 0;
+}
+
